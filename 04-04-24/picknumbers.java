3a6e67355b9c061264632e67dd2cf4c5aaf5128c
Yashwanthss2954 <yashwanthss2954@gmail.com>
2024-04-15 10:34:59 +0530

    second commit

---
+++ 04-04-24/picknumbers.java
@@ -0,0 +1,37 @@
+/*Your brother is writing his school exam and he got a mathematical problem as below, one array of nums is given to him, in which exactly two elements appear only once and all the other elements appear exactly twice, he need to find those two elements that appear only once. He has to give answer in ascending order only. Once he come back he asked you to solve  gh that problem using your programming skills in  linear runtime.
+
+Note : Each integer in nums will appear twice, only two integers will appear once.
+	   
+
+ input format :an integer n
+               n number of integers
+output format : list of two integers
+
+
+Example 1:
+input =10
+10 1 -2 1 -3 4 4 10 2 2
+output=[-3, -2]
+
+Example 2:
+
+Input= 6
+1 2 1 3 2 5
+Output: [3,5]
+Explanation:  [5, 3] is also a valid answer.
+
+Example 3:
+Input=-1 0
+Output=[-1,0]
+
+Example 4:
+Input2=2
+0 1
+Output: [1,0]
+ 
+
+Constraints:
+
+2 <= nums.length <= 3 * 104
+-231 <= nums[i] <= 231 - 1
+ */
