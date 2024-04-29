dacdb3f84060ba68ccf093c20c7db9be8210a56d
Yashwanthss2954 <yashwanthss2954@gmail.com>
2024-04-29 16:19:19 +0530

    second commit

---
+++ CP/April/29-04-24/smalleststring.java
@@ -0,0 +1,115 @@
+/*Vihaar is working with strings. 
+He is given two strings A and B, and another string T,
+where the length of A and B is same.
+
+You can find the relative groups of letters from A and B,
+using the following rule set:
+- Equality rule: 'p' == 'p'
+- Symmetric rule: 'p' == 'q' is same as 'q' == 'p'
+- Transitive rule: 'p' == 'q' and 'q' == 'r' indicates 'p' == 'r'.
+
+Vihaar has to form the relatively smallest string of T,
+using the relative groups of letters.
+
+For example, if A ="pqr" and B = "rst" , 
+then we have 'p' == 'r', 'q' == 's', 'r' == 't' .
+
+The relatives groups formed using above rule set are as follows: 
+[p, r, t] and [q,s] and  String T ="tts", then relatively smallest string is "ppq".
+
+You will be given the strings A , B and T.
+Your task is to help Vihaar to find the relatively smallest string of T.
+
+
+Input Format:
+-------------
+Three space separated strings, A , B and T
+
+Output Format:
+--------------
+Print a string, relatively smallest string of T.
+
+
+Sample Input-1:
+---------------
+kmit ngit mgit
+
+Sample Output-1:
+----------------
+ggit
+
+Explanation: 
+------------
+The relative groups using A nd B are [k, n], [m, g], [i], [t] and
+the relatively smallest string of T is "ggit"
+
+
+Sample Input-2:
+---------------
+attitude progress apriori
+
+Sample Output-2:
+----------------
+aaogoog
+
+Explanation: 
+------------
+The relative groups using A nd B are [a, p], [t, r, o], [i, g] and [u, e, d, s]
+the relatively smallest string of T is "aaogoog" */
+
+
+
+
+import java.util.*;
+
+class DisjointUnionSets {
+    int[] rank, parent;
+    int n;
+    public DisjointUnionSets(int n) {
+        this.n = n;
+        rank = new int[n];
+        parent = new int[n];
+        makeSet();
+    }
+    void makeSet() {
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+    }
+    int find(int x) {
+        if (parent[x] != x) {
+            parent[x] = find(parent[x]); 
+        }
+        return parent[x];
+    }
+    void union(int x, int y) {
+        int xRoot = find(x);
+        int yRoot = find(y);
+        if (xRoot == yRoot) return;
+        if (rank[xRoot] < rank[yRoot]) {
+            parent[xRoot] = yRoot;
+        } else if (rank[xRoot] > rank[yRoot]) {
+            parent[yRoot] = xRoot;
+        } else {
+            parent[yRoot] = xRoot;
+            rank[xRoot]++;
+        }
+    }
+}
+
+class smalleststring {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String A = sc.next();
+        String B = sc.next();
+        String T = sc.next();
+        DisjointUnionSets dsu = new DisjointUnionSets(26); 
+        for (int i = 0; i < A.length(); i++) {
+            int r1 = A.charAt(i) - 'a';
+            int r2 = B.charAt(i) - 'a';
+            dsu.union(r1, r2);
+        }
+        
+        System.out.println(result.toString());
+    }
+}
